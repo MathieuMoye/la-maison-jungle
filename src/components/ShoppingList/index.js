2fef34983ea52ba4b60010afe6b58e33924fda84
MathieuMoye <mathieu.afev66@gmail.com>
2023-03-20 15:58:31 +0100

    ternaire soldes

--- src/components/ShoppingList/index.js
+++ src/components/ShoppingList/index.js
@@ -1,22 +1,26 @@
 import { plantList } from '../../datas/plantList'
+import './styles.css'
 
 const ShoppingList = () => {
-	// const categories = plantList.reduce(
-	// 	(acc, plant) =>
-	// 		acc.includes(plant.category) ? acc : acc.concat(plant.category),
-	// 	[]
-	// )
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
 	return (
 		<div>
-			{/* <ul>
+			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
-			</ul> */}
-			<ul>
+			</ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
 				))}
 			</ul>
 		</div>
